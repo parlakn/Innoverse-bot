6d715fbdadea53f0867b83fb76bd697b7d8c3c28
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-06-25 21:32:30 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,32 @@
 const { Client, GatewayIntentBits, SlashCommandBuilder } = require('discord.js');
 const config = require('./config.json');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const client = new Client({ 
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] 
+});
 
 client.once('ready', () => {
     console.log('bot started now !');
     
     const helpCommand = new SlashCommandBuilder()
         .setName('help')
-        .setDescription('help command!');
+        .setDescription('Help command');
     
-    const guildId = '1236344772331831428';
-    const commands = [helpCommand.toJSON()];
+    const testCommand = new SlashCommandBuilder()
+        .setName('test')
+        .setDescription('test command');
+
+    const rulesCommand = new SlashCommandBuilder()
+        .setName('rules')
+        .setDescription('show discord server rules !');
+    
+    const guildId = '1236344772331831428'; 
+    const commands = [helpCommand.toJSON(), testCommand.toJSON(), rulesCommand.toJSON()];
     
     client.guilds.cache.get(guildId)?.commands.set(commands)
         .then(() => console.log(''))
         .catch(console.error);
 });
-
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
@@ -24,7 +34,11 @@ client.on('interactionCreate', async interaction => {
     const { commandName } = interaction;
 
     if (commandName === 'help') {
-        await interaction.reply('help command :D');
+        await interaction.reply('What i can do ?');
+    } else if (commandName === 'test') {
+        await interaction.reply('test');
+    } else if (commandName === 'rules') {
+        await interaction.reply('later soon..');
     }
 });
 
